e7d7fc9de39d34986c1ab295b0d6d130cf620450
Aslan Varoqua <aslan.varoqua@gmail.com>
2018-05-26 01:13:56 -0600

    gocosmic

--- gocosmic.go
+++ gocosmic.go
@@ -1,11 +1,11 @@
-
-
 package gocosmic
 
 import "github.com/gocosmic/common"
 
+//// OOP GoCosmic Type Should contain all Cosmic.js Api end points
 type GoCosmic struct { }
 
+/// POST JSON 
 func GoCosmic(url) Post(url) string {
     Common(url).Post()
     	//
@@ -25,6 +25,7 @@ func GoCosmic(url) Post(url) string {
     return res.body
 }
 
+//// GET JSON
 func GoCosmic(url) Get(url) string {
   resp = Common(url).Get()
 	//
@@ -42,3 +43,22 @@ func GoCosmic(url) Get(url) string {
 	}
 	println("")
 }
+
+
+// func GoCosmic(url) GetImage(url) string {
+//   resp = Common(url).Get()
+// 	//
+// 	// Process response
+// 	//
+// 	println("")
+// 	if resp.Status() == 201 {
+// 		fmt.Printf("CosmicJs Response:", res.body)
+// 	} else {
+// 		fmt.Println("Bad response status from CosmicJs server")
+// 		fmt.Printf("\t Status:  %v\n", resp.Status())
+// 		fmt.Printf("\t Message: %v\n", e.Message)
+// 		fmt.Printf("\t Errors: %v\n", e.Message)
+// 		pretty.Println(e.Errors)
+// 	}
+// 	println("")
+// }
